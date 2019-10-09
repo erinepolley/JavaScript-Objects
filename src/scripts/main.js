f0156e45d1be054812744a874c7add866b821cbc
Erin Polley <erinepolley@gmail.com>
2019-10-09 14:42:36 -0500

    completed exercises in chapter

---
+++ src/scripts/main.js
@@ -0,0 +1,45 @@
+console.log("Heya");
+
+const food = {
+    type: "Hamburger",
+    size: "small"
+    
+};
+
+const ford = {
+    make: "Ford",
+    model: "mustang",
+    year: 2015,
+    color: "red"
+};
+
+const animalNames = ["Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", "Sharky"];
+console.log(animalNames);
+
+const father = {
+    name: "Brad",
+    age: 36,
+    sex: "male"
+};
+
+const mother = {
+    name: "Erin",
+    age: 33, 
+    sex: "female"
+}
+
+const child = {
+    name: "Georgia",
+    age: 2,
+    sex: "female"
+}
+
+const child2 = {
+    name: "Isabel",
+    age: .5,
+    sex: "female"
+}
+
+const myFamily = [father, mother, child, child2];
+
+console.log(myFamily);
