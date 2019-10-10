3efb15f2d746c8367b0944471dae6036d25810e5
Erin Polley <erinepolley@gmail.com>
2019-10-10 10:54:10 -0500

    beatles exercise completed and correct

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -109,3 +109,38 @@ console.log(nashvilleSoftwareSchool.instructors.partTime[0], nashvilleSoftwareSc
 for (let i in nashvilleSoftwareSchool.instructors) {
     console.log(nashvilleSoftwareSchool.instructors[i]);
 }
+
+const beatles = {
+    albums: ["Abbey Road", "Sgt Peppers Lonely Heart's Club Band", "Revolver", "Magical Mystery Tour", "Something New"],
+    history: {
+        formed: 1960,
+        disbanded: 1970
+    },
+    members: [
+        {
+            name: "George Harrison",
+            birth: 1943,
+            death: 2001
+        },
+        {
+            name: "Paul McCartney",
+            birth: 1942,
+            death: null
+        },
+        {
+            name: "John Lennon",
+            birth: 1940,
+            death: 1980
+        },
+        {
+            name: "Ringo Starr",
+            birth: 1940,
+            death: null
+        }
+    ]
+}
+
+console.log(`${beatles.members[1].name} was in the Beatles from 
+${beatles.history.formed} to ${beatles.history.disbanded}. He was
+born in ${beatles.members[1].birth}. He contributed to the 
+${beatles.albums[3]}.`)
